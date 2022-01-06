5d9b29de0be720517812adbd79abf373691bdf0e
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-01-05 21:29:36 -0800

    Add some simple read-write tests

---
+++ test/read-write.test.ts
@@ -0,0 +1,17 @@
+import { tstl } from "./util"
+
+test("luaPlayer color", () => {
+  tstl`
+    declare const player: LuaPlayer
+    const r = player.color.r
+    player.color = [1, 1, 1]
+  `.expectToHaveNoDiagnostics()
+})
+
+test("mining state", () => {
+  tstl`
+    declare const player: LuaControl
+    const x = player.mining_state.position.x
+    player.mining_state = { mining: true, position: [0, 0] }
+  `.expectToHaveNoDiagnostics()
+})
