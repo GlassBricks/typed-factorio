3bb12a1997fea24738dda6999782d8873657493e
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-09-07 11:36:56 -0700

    Improve/fix "util" lualib module definitions

--- runtime/util.d.ts
+++ runtime/util.d.ts
@@ -8,54 +8,58 @@ declare module "util" {
   namespace table {
     function deepcopy<T>(table: T): T
 
-    function compare<T>(tb1: T, tb2: T): boolean
+    function compare<T extends table>(tb1: T, tb2: T): boolean
   }
 
   function copy<T>(table: T): T
 
-  function distance(position1: MapPosition, position2: MapPosition): number
+  function distance(position1: MapPosition | MapPositionArray, position2: MapPosition | MapPositionArray): number
 
-  function positiontostr(position: MapPosition): string
+  function positiontostr(position: MapPosition | MapPositionArray): string
 
   function formattime(ticks: number): string
 
   /** Supports 'rrggbb', 'rgb', 'rrggbbaa', 'rgba', 'ww', 'w' */
-  function color(hex: string): ColorTable
+  function color(hex: string): Color
 
-  function premul_color(color: Color): ColorTable
+  function premul_color(color: Color): Color
 
   function mix_color(c1: Color, c2: Color): ColorArray
 
   function multiply_color(c1: Color, n: number): ColorArray
 
   function moveposition(
-    position: MapPosition,
+    position: MapPositionArray,
     direction: defines.direction.north | defines.direction.east | defines.direction.south | defines.direction.west,
     distance: number
-  ): MapPosition
-  function moveposition(position: MapPosition, direction: defines.direction, distance: number): MapPosition | undefined
+  ): MapPositionArray
+  function moveposition(
+    position: MapPositionArray,
+    direction: defines.direction,
+    distance: number
+  ): MapPositionArray | nil
 
   function oppositedirection(direction: defines.direction): defines.direction
 
   /** Creates a new array where each element in `stripes` is duplicated `count` times */
-  function multiplystripes<T>(count: number, stripes: T[]): T[]
+  function multiplystripes<T>(count: number, stripes: readonly T[]): T[]
 
   /** Gets tile position by pixel */
-  function by_pixel(x: number, y: number): MapPosition
+  function by_pixel(x: number, y: number): MapPositionArray
 
   /** Gets tile position by pixel, hr */
-  function by_pixel_hr(x: number, y: number): MapPosition
+  function by_pixel_hr(x: number, y: number): MapPositionArray
 
   // omitted: foreach_sprite_definition
 
-  function add_shift(a: MapPositionArray | undefined, b: MapPositionArray): MapPositionArray
-  function add_shift(a: MapPositionArray, b: MapPositionArray | undefined): MapPositionArray
-  function add_shift(a: MapPositionArray | undefined, b: MapPositionArray | undefined): MapPositionArray | undefined
+  function add_shift(a: MapPositionArray | nil, b: MapPositionArray): MapPositionArray
+  function add_shift(a: MapPositionArray, b: MapPositionArray | nil): MapPositionArray
+  function add_shift(a: MapPositionArray | nil, b: MapPositionArray | nil): MapPositionArray | nil
 
   // omitted: add_shift_offset
 
-  function mul_shift(shift: MapPositionArray, scale: number | undefined): MapPositionArray
-  function mul_shift(shift: MapPositionArray | undefined, scale: number | undefined): MapPositionArray | undefined
+  function mul_shift(shift: MapPositionArray, scale: number | nil): MapPositionArray
+  function mul_shift(shift: MapPositionArray | nil, scale: number | nil): MapPositionArray | nil
 
   function format_number(amount: number, append_suffix?: boolean): string
 
@@ -68,11 +72,11 @@ declare module "util" {
    * entries are themselves tables, in which case they are recursively merged. Non-merged tables are deep-copied, so
    * that the result is brand new.
    */
-  function merge<T extends object>(tables: T[]): T
+  function merge<T extends object>(tables: readonly T[]): T
 
-  function insert_safe(entity: LuaEntity | undefined, item_dict: Record<string, number> | undefined): void
+  function insert_safe(entity: LuaEntity | nil, item_dict: Record<string, number> | nil): void
 
-  function remove_safe(entity: LuaEntity | undefined, item_dict: Record<string, number> | undefined): void
+  function remove_safe(entity: LuaEntity | nil, item_dict: Record<string, number> | nil): void
 
   function split_whitespace(string: string): string[]
 
@@ -82,7 +86,7 @@ declare module "util" {
 
   function clamp(x: number, lower: number, upper: number): number
 
-  // omitted: get_walkable_tile
+  function get_walkable_tile(): string
 
   // omitted: combine_icons
   // omitted: technology_icons. Create an issue if you really want to see these
@@ -110,7 +114,7 @@ declare module "util" {
 
   function remove_from_list<T>(list: T[], value: T): boolean
 
-  function list_to_map<T extends keyof any>(list: T[]): Record<T, true>
-  function list_to_map<T>(list: T[]): LuaTable<T, true>
-  function list_to_map<T>(list: T): LuaTable<T[keyof T], true>
+  function list_to_map<V>(list: LuaPairsIterable<any, V>): LuaSet<NonNullable<V>>
+  function list_to_map<T extends AnyNotNil>(list: T[]): LuaSet<T>
+  function list_to_map<T extends AnyNotNil>(list: T): LuaSet<NonNullable<T[keyof T]>>
 }
