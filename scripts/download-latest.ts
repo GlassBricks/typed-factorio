4cd90a124c113139ebd720342f9f0ce656294953
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-08-27 20:41:49 -0700

    Add download script for prototypes json

--- scripts/download-latest.ts
+++ scripts/download-latest.ts
@@ -1,33 +1,35 @@
 import child_process from "child_process"
-import * as fs from "fs"
+import * as fs from "fs/promises"
+import * as fss from "fs"
 import * as https from "https"
 import * as path from "path"
 import { fileURLToPath } from "url"
-
-const url = "https://lua-api.factorio.com/latest/runtime-api.json"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const destinationFolder = path.resolve(__dirname, "../generator/input")
-const destination = path.join(destinationFolder, "downloaded-runtime-api.json")
-new Promise((resolve, reject) => {
-  const file = fs.createWriteStream(destination)
-  const request = https.get(url, (response) => {
-    if (response.statusCode !== 200) {
-      return reject("Status code:" + response.statusCode)
-    }
-    response.pipe(file)
-    file.on("finish", () => {
-      file.close(resolve)
+
+async function download(stage: string) {
+  const url = `https://lua-api.factorio.com/latest/${stage}-api.json`
+  const destination = path.join(destinationFolder, `downloaded-${stage}-api.json`)
+  await new Promise((resolve, reject) => {
+    const file = fss.createWriteStream(destination)
+    const request = https.get(url, (response) => {
+      if (response.statusCode !== 200) {
+        return reject("Status code:" + response.statusCode)
+      }
+      response.pipe(file)
+      file.on("finish", () => {
+        file.close(resolve)
+      })
+    })
+    request.on("error", (err) => {
+      fs.unlink(destination).then(() => reject(err.message))
     })
   })
-  request.on("error", (err) => {
-    fs.unlink(destination, () => {})
-    reject(err.message)
-  })
-}).then(() => {
-  const contents = JSON.parse(fs.readFileSync(destination, "utf8")) as {
+
+  const contents = JSON.parse(await fs.readFile(destination, "utf8")) as {
     application: string
     stage: string
     application_version: string
@@ -35,7 +37,7 @@ new Promise((resolve, reject) => {
   }
   const expected = {
     application: "factorio",
-    stage: "runtime",
+    stage,
     // api_version: 3,
   }
   for (const [k, value] of Object.entries(expected)) {
@@ -45,20 +47,23 @@ new Promise((resolve, reject) => {
     }
   }
   const version = contents.application_version
-  console.log(`downloaded runtime api for version ${version}`)
+  console.log(`downloaded ${stage} api for version ${version}`)
   // delete all previous versions
-  fs.readdirSync(destinationFolder)
-    .filter((file) => /runtime-api-\d+\.\d+\.\d+\.json$/.test(file))
-    .forEach((file) => {
-      fs.unlinkSync(path.join(destinationFolder, file))
-    })
-  const resultFile = path.resolve(destinationFolder, `runtime-api-${version}.json`)
-  fs.renameSync(destination, resultFile)
+  for (const file of (await fs.readdir(destinationFolder)).filter((file) =>
+    new RegExp(`${stage}-api-d+.d+.d+.json$`).test(file)
+  )) {
+    await fs.unlink(path.join(destinationFolder, file))
+  }
+  const resultFile = path.resolve(destinationFolder, `${stage}-api-${version}.json`)
+  await fs.rename(destination, resultFile)
+}
 
-  // add to git
-  const projectDir = path.resolve(__dirname, "..")
-  child_process.spawnSync("git", ["add", path.relative(projectDir, resultFile)], {
-    cwd: projectDir,
-    stdio: "inherit",
-  })
+const stages = ["runtime", "prototype"]
+await Promise.all(stages.map(download))
+
+// add to git
+const projectDir = path.resolve(__dirname, "..")
+child_process.spawnSync("git", ["add", path.relative(projectDir, destinationFolder)], {
+  cwd: projectDir,
+  stdio: "inherit",
 })
