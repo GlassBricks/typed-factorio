11579c30ade4c1d45bafecf4099d7f843a6be78a
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-11-15 06:13:57 +0000

    Update to Factorio API version 2.0.18

--- runtime/generated/global-functions.d.ts
+++ runtime/generated/global-functions.d.ts
@@ -7,12 +7,12 @@ import type { LocalisedString, table, uint } from "factorio:runtime"
 declare global {
   /**
    * `log()` can print {@link LocalisedString LocalisedStrings} to the Factorio {@linkplain https://wiki.factorio.com/Log_file log file}. This, in combination with the serpent library, makes debugging in the data stage easier because it allows the inspection of entire prototype tables. For example, printing all properties of the sulfur item prototype can be done like so: `log(serpent.block(data.raw["item"]["sulfur"]))`
-   * @see {@link https://lua-api.factorio.com/2.0.17/auxiliary/libraries.html#new-functions Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.18/auxiliary/libraries.html#new-functions Online documentation}
    */
   function log(string: LocalisedString): void
   /**
    * `localised_print()` allows printing {@link LocalisedString} to stdout without polluting the Factorio {@linkplain https://wiki.factorio.com/Log_file log file}. This is primarily useful when communicating with external tools that launch Factorio as a child process.
-   * @see {@link https://lua-api.factorio.com/2.0.17/auxiliary/libraries.html#new-functions Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.18/auxiliary/libraries.html#new-functions Online documentation}
    */
   function localised_print(string: LocalisedString): void
   /**
@@ -29,7 +29,7 @@ declare global {
    * ```
    *
    * Note that `table_size()` does not work correctly for {@link LuaCustomTable}, their size has to be determined with {@link LuaCustomTable#length LuaCustomTable::length_operator} instead.
-   * @see {@link https://lua-api.factorio.com/2.0.17/auxiliary/libraries.html#new-functions Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.18/auxiliary/libraries.html#new-functions Online documentation}
    */
   function table_size(table: table): uint
 }
