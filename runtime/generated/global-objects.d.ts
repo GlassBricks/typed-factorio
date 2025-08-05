055387a36c32812463767289c75d9c7d61c75c37
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2025-08-05 06:18:58 +0000

    Update to Factorio API version 2.0.63

--- runtime/generated/global-objects.d.ts
+++ runtime/generated/global-objects.d.ts
@@ -15,38 +15,38 @@ import type {
 declare global {
   /**
    * Allows registration of custom commands for the in-game console.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const commands: LuaCommandProcessor
   /**
    * The main scripting interface through which most of the API is accessed.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const game: LuaGameScript
   //The "settings" and "helpers" globals are declared in common/settings-global.d.ts and common/helpers.d.ts.
   /**
    * Allows read-only access to prototypes.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const prototypes: LuaPrototypes
   /**
    * Allows printing messages to the calling RCON instance, if any.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const rcon: LuaRCON
   /**
    * Allows registration and use of functions to communicate between mods.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const remote: LuaRemote
   /**
    * Allows rendering of geometric shapes, text and sprites in the game world.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const rendering: LuaRendering
   /**
    * Provides an interface for registering game event handlers.
-   * @see {@link https://lua-api.factorio.com/2.0.62/index-runtime.html Online documentation}
+   * @see {@link https://lua-api.factorio.com/2.0.63/index-runtime.html Online documentation}
    */
   const script: LuaBootstrap
   //The "settings" and "helpers" globals are declared in common/settings-global.d.ts and common/helpers.d.ts.
